65539f6c60da994f1f1ba348a7048bff13ca9383
Samuel <samuel.santiago@ufu.br>
2020-07-03 11:51:56 -0300

    fix insert consult

--- api/actions/insertConsult.php
+++ api/actions/insertConsult.php
@@ -17,36 +17,5 @@ if (!$consult->return['code']) {
   $consult->return = null;
 }
 $inserted = $consult->insert();
-$consult->id = $inserted['code'];
-$exams = [
-  [
-    'desc' => 'Exame qualquer que vai ser passado pelo form 1',
-    'qtd' => 2,
-    'type' => 'F'
-  ],
-  [
-    'desc' => 'Exame qualquer que vai ser passado pelo form 2',
-    'qtd' => 1,
-    'type' => 'U'
-  ]
-];
-$success = true;
-$exams_error = [];
-foreach ($exams as $exam) {
-  $e = new Classes\Exam;
-  $e->description = $exam['desc'];
-  $e->consult = $consult;
-  $e->qtd = $exam['qtd'];
-  $e->type = $exam['type'];
-  $result = $e->insert();
-  if (!$result || $result['code'] == 0) {
-    array_push($exams_error, $exam);
-    $success = false;
-  }
-}
-if (!$success) {
-  $inserted['msg'] = 'Erro ao adicionar '.count($exams_error).' exames';
-  $inserted['exams'] = $exams_error;
-}
 echo Classes\Base\Parse::toJson($inserted);
 ?>
